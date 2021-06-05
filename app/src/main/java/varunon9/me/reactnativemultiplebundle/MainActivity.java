eb6360e523e01652a37f458cb805db8efc91ef97
varunon9 <varunon9@gmail.com>
2021-06-05 21:15:49 +0530

    created project using Android Studio

---
+++ app/src/main/java/varunon9/me/reactnativemultiplebundle/MainActivity.java
@@ -0,0 +1,14 @@
+package varunon9.me.reactnativemultiplebundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
